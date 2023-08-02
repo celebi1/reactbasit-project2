5b106c8fa57c21571132469f091a1f532bbdaaff
Celebi <clbceri@gmail.com>
2023-08-02 15:37:44 +0300

    Basit resim galerisi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Images from './Images';
+import "./styles.css";
 
 function App() {
+
+const [selectedImg,setSelectedImg]=useState(Images[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={selectedImg} alt="Selected" className="selected"/>
+      </div>
+      <div className="imgContainer">
+        {Images.map((img,index) =>(
+          <img key={index} src={img} alt='tantuni'
+          onClick={() => setSelectedImg(img)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
